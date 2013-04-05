eec643f2ae9585e25b132e9bfec6c65972dae5f1
João Batista Neto <neto.joaobatista@gmail.com>
2013-04-04 23:36:17 -0300

    Preparando repositório

---
+++ bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+$bootstrapPath = dirname(realpath(__FILE__)). DIRECTORY_SEPARATOR;
+
+set_include_path(
+    implode(
+        PATH_SEPARATOR,
+        array_unique(
+            array_merge(
+                array(
+                    $bootstrapPath . 'src',
+                    $bootstrapPath . 'tests',
+                ),
+                explode(PATH_SEPARATOR, get_include_path())
+            )
+        )
+    )
+);
+
+spl_autoload_register(
+    function ($class)
+    {
+        $classPath = stream_resolve_include_path(
+            str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
+
+        if ($classPath !== false) {
+            require $classPath;
+        }
+    }
+);
