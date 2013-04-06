9364ec304c3fbbbf174de9a50011d7af34866d26
Antonio Spinelli <antonio.spinelli@westwing.com.br>
2013-04-05 22:05:45 -0300

    Interpretar dezenas e unidades

--- src/LendoUmCheque/Interpreter.php
+++ src/LendoUmCheque/Interpreter.php
@@ -1,30 +1,42 @@
 <?php
 namespace LendoUmCheque;
 
-class Interpreter 
+class Interpreter
 {
-    
+
+    /**
+     * @var Tokenizer
+     */
     private $tokenizer;
 
     private $matriz = array(
-        'zero'   => 0,
-        'um'     => 1,
-        'dois'   => 2,
-        'três'   => 3,
+        'zero' => 0,
+        'um' => 1,
+        'dois' => 2,
+        'três' => 3,
         'quatro' => 4,
-        'cinco'  => 5
+        'cinco' => 5,
+        'onze' => 11,
+        'vinte' => 20,
     );
 
-    public function __construct($tokenizer) 
+    public function __construct($tokenizer)
     {
-
         $this->tokenizer = $tokenizer;
     }
 
     public function interpret()
     {
+        $number = 0;
 
-    	return $this->matriz[$this->tokenizer->current()];
+        while ($token = $this->tokenizer->current()) {
+            $this->tokenizer->next();
+            if ($token == 'e') {
+                continue;
+            }
+            $number += $this->matriz[$token];
+        }
+        return $number;
     }
 
 }
--- tests/LendoUmCheque/InterpreterTest.php
+++ tests/LendoUmCheque/InterpreterTest.php
@@ -4,23 +4,37 @@ namespace LendoUmCheque;
 class InterpreterTest extends \PHPUnit_Framework_TestCase
 {
 
-	
-    
+
     public function testCanCreateAnInstanceOfInterpreter()
     {
-        $tokenizer   = new Tokenizer('dois mil quinhentos e vinte e três reais e dezoito centavos');
+        $tokenizer = new Tokenizer('dois mil quinhentos e vinte e três reais e dezoito centavos');
         $interpreter = new Interpreter($tokenizer);
 
         $this->assertInstanceOf('LendoUmCheque\Interpreter', $interpreter);
     }
 
-    public function testReturnOfMatrizNumberMilhar(){
-        $tokenizer   = new Tokenizer('dois');
+    public function testReturnOfMatrizNumberMilhar()
+    {
+        $tokenizer = new Tokenizer('dois');
         $interpreter = new Interpreter($tokenizer);
 
-        	$this->assertEquals(2, $interpreter->interpret());
+        $this->assertEquals(2, $interpreter->interpret());
     }
-    
 
+    public function testInterpretElevenToNumber()
+    {
+        $tokenizer = new Tokenizer('onze');
+        $interpreter = new Interpreter($tokenizer);
+
+        $this->assertEquals(11, $interpreter->interpret());
+    }
+
+    public function testInterpretTwentyOneToNumber()
+    {
+        $tokenizer = new Tokenizer('vinte e um');
+        $interpreter = new Interpreter($tokenizer);
+
+        $this->assertEquals(21, $interpreter->interpret());
+    }
 
 }
