67e9192605785005d0d5e7a55b809a002a770487
Andrey Knupp Vital <andreykvital@gmail.com>
2013-04-07 03:44:16 -0300

    Adicionando novo teste e modificando o uso de Interpreter::interpret()

--- tests/LendoUmCheque/InterpreterTest.php
+++ tests/LendoUmCheque/InterpreterTest.php
@@ -16,57 +16,64 @@ class InterpreterTest extends \PHPUnit_Framework_TestCase
     public function testReturnOfMatrizNumberMilhar()
     {
         $tokenizer = new Tokenizer('dois');
-        $interpreter = new Interpreter($tokenizer);
+        $interpreter = new Interpreter();
 
-        $this->assertEquals(2, $interpreter->interpret());
+        $this->assertEquals(2, $interpreter->interpret($tokenizer));
     }
 
     public function testInterpretElevenToNumber()
     {
         $tokenizer = new Tokenizer('onze');
-        $interpreter = new Interpreter($tokenizer);
+        $interpreter = new Interpreter();
 
-        $this->assertEquals(11, $interpreter->interpret());
+        $this->assertEquals(11, $interpreter->interpret($tokenizer));
     }
 
     public function testInterpretTwentyOneToNumber()
     {
         $tokenizer = new Tokenizer('vinte e um');
-        $interpreter = new Interpreter($tokenizer);
+        $interpreter = new Interpreter();
 
-        $this->assertEquals(21, $interpreter->interpret());
+        $this->assertEquals(21, $interpreter->interpret($tokenizer));
     }
 
     public function testInterpretFiveHundredSixtyOneToNumber()
     {
         $tokenizer = new Tokenizer('quinhentos e sessenta e um');
-        $interpreter = new Interpreter($tokenizer);
+        $interpreter = new Interpreter();
 
-        $this->assertEquals(561, $interpreter->interpret());
+        $this->assertEquals(561, $interpreter->interpret($tokenizer));
     }
 
     public function testInterpretTwentyFiveThousandsToNumber()
     {
         $tokenizer = new Tokenizer('vinte e cinco mil');
-        $interpreter = new Interpreter($tokenizer);
+        $interpreter = new Interpreter();
 
-        $this->assertEquals(25000, $interpreter->interpret());
+        $this->assertEquals(25000, $interpreter->interpret($tokenizer));
     }
 
     public function testInterpretOneThousandFourHundredThirtyThreeToNumber()
     {
         $tokenizer = new Tokenizer('um mil e quatrocentos e trinta e três');
-        $interpreter = new Interpreter($tokenizer);
+        $interpreter = new Interpreter();
 
-        $this->assertEquals(1433, $interpreter->interpret());
+        $this->assertEquals(1433, $interpreter->interpret($tokenizer));
     }
 
     public function testInterpretTwoMillionOneThousandFourHundredThirtyThreeToNumber()
     {
         $tokenizer = new Tokenizer('dois milhões e quinhentos e um mil e quatrocentos e trinta e três');
-        $interpreter = new Interpreter($tokenizer);
+        $interpreter = new Interpreter();
 
-        $this->assertEquals(2501433, $interpreter->interpret());
+        $this->assertEquals(2501433, $interpreter->interpret($tokenizer));
+    }
+    
+    public function testInterpretTwoThousandFiveHundredTwentyThreeEighteenCentsToNumber()
+    {
+        $interpreter= new Interpreter();
+        $tokenizer = new Tokenizer('dois mil quinhentos e vinte e três reais e dezoito centavos');
+        $this->assertEquals(2523.18, $interpreter->interpret($tokenizer));
     }
 
 }
